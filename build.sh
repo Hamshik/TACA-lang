e8ba1eef192e0dd80b694d78a20c48d9a416d6b0
Hamshik <hamshik.sbv3@gmail.com>
2026-03-15 15:54:52 +0530

    remove mit

--- build.sh
+++ build.sh
@@ -24,7 +24,6 @@ gcc -Wall -Wextra -g -Isrc \
     $AST_DIR/ASTHelper.c \
     $AST_DIR/assigner.c \
     $AST_DIR/env.c \
-    src/codegen/scm_codegen.c \
     $SEMAN/semanic.c \
     $SEMAN/symbol_table.c \
     $UTILS/printers/token_printer.c \
--- src/main.c
+++ src/main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include "ast/ASTNode.h"
 #include "parser/parser.h"
 #include "semantic/semantic.h"
 #include "eval/eval.h"
-#include "codegen/scm_codegen.h"
 
-int write_file(const char *path) {
-    FILE *f = fopen(path, "w");   // "w" = create/truncate for writing
-    if (!f) return 0;
-
-    fprintf(f, "hello\n");
-    fclose(f);
-    return 1;
-}
-
-int main(int argc, char **argv) {
+int main(void) {
     yyparse();
     if (root != NULL) {
-        if (argc > 1 && strcmp(argv[1], "--emit-scm") == 0) {
-            semantic_check(root);
-
-            char *scm = scm_codegen(root);
-            fputs(scm, stdout);
-            write_file("docs/output.scm");
-            free(scm);
-        
-        } else {
-            semantic_check(root);
-            ast_eval(root);
-        }
+        semantic_check(root);
+        ast_eval(root);
         ast_free(root);
         env_clear_all();
     }
